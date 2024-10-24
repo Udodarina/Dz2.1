a6ce603253645ac52a2e8e287992fc2278381b19
Arina Udod <audod6150@gmail.com>
2024-10-24 18:37:11 +0300

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
         System.out.print("Введите сумму для пополнения баланса: ");
 
-        int summ = scanner.nextInt();
+        Scanner input = new Scanner(System.in);
+        int summ = input.nextInt();
 
         if (summ > 1000) {
-            int sto = (int) (summ - 1000);
-            int bonus = (int) (sto / 100);
+            int hundred = (int) (summ - 1000);
+            int bonus = (int) (hundred / 100);
             summ = (summ + bonus);
-            System.out.println("Ваш баланс:" + " " + summ);
+            System.out.println("Ваш баланс:" + summ);
         } else {
-            System.out.println("Ваш баланс:" + " " + summ);
+            System.out.println("Ваш баланс:" + summ);
         }
     }
 }
